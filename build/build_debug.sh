fa076c941af40ff07542bbfc3c0d825dfa385ee9
chenkuanglu <chenkuanglu@qq.com>
2019-05-25 17:16:41 +0800

    -

--- build/build_debug.sh
+++ build/build_debug.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
 cmake -DCMAKE_BUILD_TYPE=Debug ..
+make
+if [ ! -x bin ]; then
+    mkdir bin
+fi
+if [ ! -x lib ]; then
+    mkdir lib
+fi
+cp app/trigg_miner/trigg bin
+cp libclib.a lib
+
