1427f065e86b5e34c3ea08f03fb6fdf674d213d0
chenkuanglu <chenkuanglu@qq.com>
2019-06-18 19:13:32 +0800

    -

--- app/test_que/main.c
+++ app/test_que/main.c
@@ -82,3 +82,85 @@ void app_proper_exit(int ec)
 }
 #endif 
 
+
+
+//////////////////////////////////////////////////////
+
+typedef int (*socket_recv_callback_t)(struct sockaddr_in *src_addr, void *data, int len);
+typedef struct {
+    mux_t lock;
+	struct sockaddr_in addr_listen;
+    socket_recv_callback_t recv_proc;
+} netcom_t;
+
+int online_proc(struct sockaddr_in *src_addr, void *data, int len)
+{
+    if (len > 0) {
+        logw("data from %s:%d: %s", inet_ntoa(src_addr->sin_addr), src_addr->sin_port, (char*)data);
+    }
+    return 0;
+}
+
+int socket_set_addr(struct sockaddr_in *saddr, uint32_t ip, uint16_t port)
+{
+    bzero(saddr, sizeof(struct sockaddr_in));
+    saddr->sin_family = AF_INET;
+    saddr->sin_addr.s_addr = htonl(ip);
+    saddr->sin_port = htons(port);
+    return 0;
+}
+
+int socket_netcom_init(netcom_t *netcom)
+{
+    return mux_init(&netcom->lock);
+}
+
+int socket_recv(netcom_t *netcom, void *buf, int len)
+{
+    int opt_en = 1;
+    int fd_socket = -1;
+    socklen_t slen = sizeof(struct sockaddr_in);
+    if ((fd_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        return -1;
+    }
+    if (setsockopt(fd_socket, SOL_SOCKET, SO_BROADCAST | SO_REUSEADDR, (char *)&opt_en, sizeof(opt_en)) < 0) {
+        close(fd_socket);
+        return -1;
+    }
+	struct sockaddr_in addr_src;
+	if (bind(fd_socket, (struct sockaddr *)&(netcom->addr_listen), slen) == -1) {
+        close(fd_socket);
+        return -1;
+    }
+	
+    for (;;) {
+        int ret = recvfrom(fd_socket, buf, len, 0, (struct sockaddr *)&(addr_src), &slen);
+        if (ret < 0) {
+            close(fd_socket);
+            return -1;
+        }
+        netcom->recv_proc(&addr_src, buf, ret);
+    }
+}
+
+
+// 等待PC管理工具发送心跳包
+void *thread_online_listen(void *unused)
+{
+    char buf[2048] = {0};
+    netcom_t netcom;
+    socket_netcom_init(&netcom);
+    socket_set_addr(&netcom.addr_listen, INADDR_ANY, PORT_ONLINE_NOTIFY);
+    netcom.recv_proc = online_proc;
+
+    for (;;) {
+        if (socket_recv(&netcom, buf, sizeof(buf)) < 0) {
+            loge("socket recv fail: %s", strerror(errno));
+            usleep(100*1000);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////
+
+
