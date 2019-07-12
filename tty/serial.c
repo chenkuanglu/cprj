5fb82f27a47cc91a8f5156c10f8827a60d9cf85b
chenkuanglu <chenkuanglu@qq.com>
2019-07-12 13:42:22 +0800

    -

--- tty/serial.c
+++ tty/serial.c
@@ -54,6 +54,32 @@ static int set_speed(int fd, int speed)
 }
 
 
+// c_cflag: 
+// CLOCAL 本地模式，不改变端口的所有者
+// CREAD 表示使能数据接收器
+//
+// PARENB 表示奇偶校验使能
+// ~PARODD 表示偶校验, |PARODD 表示奇校验
+//
+// CSTOPB 使用两个停止位
+// CSIZE 对数据的bit使用掩码
+// CS8 数据宽度是8bit
+//
+// c_iflag:
+// ICANON 使能规范输入，否则使用原始数据
+// ECHO 回送(echo)输入数据
+// ECHOE 回送擦除字符
+// ISIG 使能SIGINTR，SIGSUSP， SIGDSUSP和 SIGQUIT 信号
+//
+// IXON 使能输出软件控制
+// IXOFF 使能输入软件控制
+// IXANY 允许任何字符再次开启数据流
+// INLCR 把字符NL(0A)映射到CR(0D)
+// IGNCR 忽略字符CR(0D)
+// ICRNL 把CR(0D)映射成字符NR(0A)
+// c_oflag： OPOST 输出后处理，如果不设置表示原始数据（本文使用原始数据）
+// c_cc[VMIN]： 最少可读数据
+// c_cc[VTIME]： 等待数据时间(10秒的倍数)
 static int set_parity(int fd, int databits, int stopbits, int parity)
 {
     struct termios options;
@@ -92,14 +118,15 @@ static int set_parity(int fd, int databits, int stopbits, int parity)
 		break;
 	case 'o':
 	case 'O':
-		options.c_cflag |= (PARODD | PARENB);   /* odd */ 
-		options.c_iflag |= INPCK;               /* Disnable parity checking */
+		options.c_cflag |= PARENB;      /* Enable parity */
+		options.c_cflag |= PARODD;      /* odd */ 
+		options.c_iflag |= INPCK | ISTRIP;       /* Enable parity checking on input */
 		break;
 	case 'e':
 	case 'E':
 		options.c_cflag |= PARENB;      /* Enable parity */
-		options.c_cflag &= ~PARODD;       
-		options.c_iflag |= INPCK;       /* Disnable parity checking */
+		options.c_cflag &= ~PARODD;     /* even */ 
+		options.c_iflag |= INPCK | ISTRIP;       /* Enable parity checking on input */
 		break;
 	case 'S':
 	case 's':                           /* as no parity*/
@@ -138,6 +165,13 @@ static int set_parity(int fd, int databits, int stopbits, int parity)
     return 0;
 }
  
+//
+// 非阻塞：
+// O_NONBLOCK, O_NDELAY:
+// 它们的差别在于：在读操作时，如果读不到数据，O_NDELAY会使I/O函数马上返回0，但这又衍生出一个问题，
+// 因为读取到文件末尾(EOF)时返回的也是0，这样无法区分是哪种情况。
+// 因此，O_NONBLOCK就产生出来，它在读取不到数据时会回传-1，并且设置errno为EAGAIN
+//
 int ser_open(const char* device, int speed)
 {
 	int fd = open(device, O_RDWR | O_NOCTTY);
